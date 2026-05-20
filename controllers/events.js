3c729bedfa3700172007a2daf0060ad98389bbc2
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-05-20 17:04:49 +0200

    Add put and delete functions to controller

--- controllers/events.js
+++ controllers/events.js
@@ -5,6 +5,7 @@ export async function getAllEvents(req, res) {
   try {
     const db = getDb();
     const events = await db.collection('events').find().toArray();
+
     res.status(200).json(events);
   } catch (error) {
     res.status(500).send(error.message);
@@ -16,6 +17,7 @@ export async function getSingleEvent(req, res) {
     if (!ObjectId.isValid(req.params.id)) {
       return res.status(400).json({ message: 'Invalid ID format.' });
     }
+
     const db = getDb();
     const event = await db
       .collection('events')
@@ -46,10 +48,64 @@ export async function createEvent(req, res) {
     };
 
     const response = await db.collection('events').insertOne(newEvent);
+
     if (response.acknowledged) {
       res.status(201).json(response);
     } else {
       res.status(500).json({ message: 'Creation failed.' });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function updateEvent(req, res) {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid ID format.' });
+    }
+
+    const db = getDb();
+    const eventId = new ObjectId(req.params.id);
+    const updatedEvent = {
+      name: req.body.name,
+      description: req.body.description,
+      date: req.body.date,
+      location: req.body.location,
+      capacity: req.body.capacity,
+      ticketPrice: req.body.ticketPrice,
+      category: req.body.category,
+      isVirtual: req.body.isVirtual,
+    };
+    const response = await db
+      .collection('events')
+      .replaceOne({ _id: eventId }, updatedEvent);
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Event not found or no changes made.' });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function deleteEvent(req, res) {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid ID format.' });
+    }
+
+    const db = getDb();
+    const response = await db
+      .collection('events')
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Event not found.' });
     }
   } catch (error) {
     res.status(500).send(error.message);
