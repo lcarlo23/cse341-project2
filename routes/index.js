a2b7afd124e5c35768b5bce546f15355988a9235
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-05-20 16:55:09 +0200

    Add locations route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import eventRoutes from './events.js';
+import locationRoutes from './locations.js';
 
 const router = express.Router();
 
 router.use('/events', eventRoutes);
+router.use('/locations', locationRoutes);
 
 router.get('/', (req, res) => {
   res.send('Event Planner API');
